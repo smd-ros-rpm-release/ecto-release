f3bb57ce968626e1d46d5f4965aa613043ee97e3
Vincent Rabaud <vrabaud@willowgarage.com>
2014-01-26 15:49:14 +0100

    Rebase from 'upstream'

--- src/lib/except.cpp
+++ src/lib/except.cpp
@@ -172,8 +172,12 @@ namespace ecto
           for( error_info_map::const_iterator i=info_.begin(),end=info_.end(); i!=end; ++i )
             {
               boost::shared_ptr<error_info_base const> const & x = i->second;
+#if ((BOOST_VERSION / 100) % 1000) <= 50
               tmp << str(fmt % /*name_of(*/x->tag_typeid_name()/*) */
                          % x->value_as_string());
+#else
+              tmp << boost::str(fmt % x->name_value_string());
+#endif
             }
           tmp.str().swap(diagnostic_info_str_);
         }
